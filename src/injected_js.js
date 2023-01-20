c26c4426ef9b62d29dc62b0f7e59a41ae2f3b41b
Sridaran Thoniyil <sri7thon@gmail.com>
2023-01-20 17:00:26 -0500

    Add optional makeBackgroundDark parameter for highlight

--- src/injected_js.js
+++ src/injected_js.js
@@ -8,14 +8,24 @@ const sleep = (milliseconds) => {
 const dimOverlayDiv = document.createElement("div");
 document.querySelector("body").appendChild(dimOverlayDiv);
 
-const highlight = async (selector) => {
+const highlight = async (selector, makeBackgroundDark) => {
   dimOverlayDiv.classList.add("dim-overlay");
+
+  if (makeBackgroundDark) {
+    dimOverlayDiv.classList.add("dark-overlay");
+  }
+
   document.querySelector(selector).classList.add("dim-overlay-highlight");
   await sleep(20);
 };
 
-const unhighlight = async (selector) => {
+const unhighlight = async (selector, wasBackgroundDark) => {
   dimOverlayDiv.classList.remove("dim-overlay");
+
+  if (wasBackgroundDark) {
+    dimOverlayDiv.classList.remove("dark-overlay");
+  }
+
   document.querySelector(selector).classList.remove("dim-overlay-highlight");
   await sleep(20);
 };
